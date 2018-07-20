9ea15283b77c5d66ac27d0011e03df5d5095dc2b
Daigo Hirooka <daigo@pluto.murata.eb.waseda.ac.jp>
2018-07-20 18:48:13 +0900

    few change

--- loss.py
+++ loss.py
@@ -2,28 +2,20 @@ import tensorflow as tf
 import pdb
 
 def sparse_softmax_cross_entropy2d(labels2d, logits2d, name = 'loss'):
-    
-    # shape_labels = tf.shape(labels2d) # (#batches, h, w)
-    # shape_logits = tf.shape(logits2d) # (#batches, h, w, #classes)
-    # for i, s_labels in enumerate(shape_labels):
-    #     assert s_labels == shape_logits[i], 'labels and logits must have same shapes'
-
-    # n_classes = shape_logits[-1] # channel last
-        
     with tf.name_scope(name) as ns:
         n_classes = tf.shape(logits2d)[-1]
-        
+
         labels = tf.reshape(labels2d, [-1]) # shape(#batchesxhxw)
-        labels_onehot = tf.one_hot(labels, depth = n_classes,
+        labels_onehot = tf.one_hot(labels, depth = n_classes, axis = -1,
                                    dtype = tf.float32) # shape(#batchesxhxw, #classes)
 
         logits = tf.reshape(logits2d, [-1, n_classes]) # shape(#batchesxhxw, #classes)
 
         loss = tf.losses.softmax_cross_entropy(labels_onehot, logits)
         
-        preds = tf.nn.softmax(logits, axis = 1)
+        preds = tf.nn.softmax(logits, axis = -1)
         accuracy = tf.reduce_mean(tf.reduce_sum(labels_onehot*preds,
-                                                axis = 1))
+                                                axis = -1))
         labels2d_pred = tf.argmax(logits2d, axis = -1)
         
         return loss, accuracy, labels2d_pred
